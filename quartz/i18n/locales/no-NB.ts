f86b26c8c09aed89931f2bdf970384f8c5363648
strombraaten <ssb@variant.no>
2024-02-06 14:49:17 +0100

    Added norwegian translation

---
+++ quartz/i18n/locales/no-NB.ts
@@ -0,0 +1,65 @@
+import { Translation } from "./definition"
+
+export default {
+  propertyDefaults: {
+    title: "Ikke navngitt",
+    description: "Mangler beskrivelse",
+  },
+  components: {
+    backlinks: {
+      title: "Andre notater som linker tilbake hit",
+      noBacklinksFound: "Fant ingen notater som linker tilbake",
+    },
+    themeToggle: {
+      lightMode: "Lyse omgivelser",
+      darkMode: "Mørke omgivelser",
+    },
+    explorer: {
+      title: "Alle notater",
+    },
+    footer: {
+      createdWith: "Lagd med",
+    },
+    graph: {
+      title: "Notatkart",
+    },
+    recentNotes: {
+      title: "Nylig oppdaterte notater",
+      seeRemainingMore: ({ remaining }) => `Se ${remaining} til →`,
+    },
+    transcludes: {
+      transcludeOf: ({ targetSlug }) => `Utdrag av ${targetSlug}`,
+      linkToOriginal: "Link til det originale notatet",
+    },
+    search: {
+      title: "Søk",
+      searchBarPlaceholder: "Søk etter noe",
+    },
+    tableOfContents: {
+      title: "Hva du finner i notatet",
+    },
+  },
+  pages: {
+    rss: {
+      recentNotes: "Nylig oppdaterte notater",
+      lastFewNotes: ({ count }) => `Siste ${count} notater`,
+    },
+    error: {
+      title: "Fant ingenting",
+      notFound: "Beklager, nå har jeg sendt deg på en blindvei.",
+    },
+    folderContent: {
+      folder: "Mappe",
+      itemsUnderFolder: ({ count }) =>
+        count === 1 ? "Ett notat i denne mappa" : `${count} notater i denne mappa.`,
+    },
+    tagContent: {
+      tag: "Emneknagg",
+      tagIndex: "Oversikt over emneknagger",
+      itemsUnderTag: ({ count }) =>
+        count === 1 ? "Ett notat med denne emneknaggen" : `${count} notater med denne emneknaggen.`,
+      showingFirst: ({ count }) => `Viser første ${count} emneknagger.`,
+      totalTags: ({ count }) => `Fant ${count} emneknagger totalt.`,
+    },
+  },
+} as const satisfies Translation
