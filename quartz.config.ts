b2c6299f929c984aa1e9bcaca13c790edea5c3b7
strombraaten <ssb@variant.no>
2024-04-01 17:52:51 +0200

    changed the defaultDateType

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "nb-NO",
     baseUrl: "https://simenskriver.no",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
