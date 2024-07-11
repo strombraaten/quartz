767f17868de5c1a81883ae31b6b1cad69db1d22e
strombraaten <ssb@variant.no>
2024-07-11 11:02:06 +0200

    adding recentnotes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,9 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.RecentNotes(),
+    ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/strombraaten/quartz",
