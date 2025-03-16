abdddc9c2327effd87ba2342024bc598ab3e16ca
strombraaten <ssb@variant.no>
2025-03-16 23:37:48 +0100

    edited the title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Simen skriver",
+    pageTitle: "Simen skriver",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
