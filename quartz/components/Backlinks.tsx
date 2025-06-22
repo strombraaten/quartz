1175238c29ea6eab7a86e0ee32c200c8e1d792b5
strombraaten <ssb@variant.no>
2025-06-22 08:28:27 +0200

    trying to hide the index page from backlinks

--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -24,7 +24,8 @@ export default ((opts?: Partial<BacklinksOptions>) => {
     cfg,
   }: QuartzComponentProps) => {
     const slug = simplifySlug(fileData.slug!)
-    const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug))
+    const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug) && file.slug !== "index"
+  )
     if (options.hideWhenEmpty && backlinkFiles.length == 0) {
       return null
     }
